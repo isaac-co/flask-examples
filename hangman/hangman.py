9648ad3ab4011aa449a8f9c3efac3a35291d8e8e
Isaac Cortes <isaacco_@outlook.com>
2022-05-11 13:31:21 -0500

    Changed logic, added only lowercase

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = "key"
 
 # TODO:
-# - Always lowercase 
+# - Always lowercase DONE
 # - JSX for Game Over in hangman.html
 # - End game when word is guessed
 # - End game after 6 tries
@@ -30,7 +30,7 @@ def index():
 
 @app.route('/hangman', methods=['POST'])
 def init():
-    session['word'] = request.form.get('word', None)
+    session['word'] = request.form.get('word', None).lower()
     session['used'] = []
     session['tries'] = 0
     # Format
@@ -40,14 +40,14 @@ def init():
 
 @app.route('/hangman/', methods=['POST'])
 def hangman():
+    # if unsuccessful:
     session['tries'] += 1
-    if session['tries'] < 6:
-        session['used'].append(request.form.get('letter', None))
-        # Format
-        word = showWord(session['used'],session['word'])
-        used = ' '.join(session['used'])
-        return render_template('hangman.html', spaces=word, used=used, tries=session["tries"])
+    session['used'].append(request.form.get('letter', None).lower())
+    # Format
+    word = showWord(session['used'],session['word'])
     used = ' '.join(session['used'])
+    if session['tries'] < 6:
+        return render_template('hangman.html', spaces=word, used=used, tries=session["tries"])
     return render_template('hangman.html', spaces='Game over', used=used, tries=6)
     
 
